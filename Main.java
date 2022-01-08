15f978a3cadda144ff9a9281f74d7ac831ab7d3d
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2022-01-08 16:32:08 -0300

    Organização do Projeto

--- Main.java
+++ Main.java
@@ -1,12 +1,33 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+
+   //VARIÁVEIS:
+   static String nomeJogador1, nomeJogador2;
+
 
    public static void limparTela() throws IOException, InterruptedException {
       new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
    }
 
+   public static void obterNomeDoJogador() {
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println("Digite o seu nome: ");
+      nomeJogador1 = input.next();
+      nomeJogador2 = "Computador";
+
+      while (nomeJogador1.equals(nomeJogador2)) {
+         System.out.println("");
+         System.out.println("O seu nome não pode ser o mesmo da máquina!!!");
+         System.out.println("Digite um nome diferente:");
+         nomeJogador2 = input.next();
+      }
+   }
+
    public static void main(String[] args) throws Exception {
       limparTela();
+      obterNomeDoJogador();
    }
 }
