32a2167a86d730e7b88e4be03f7f1b6d3962cdc7
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2022-01-10 16:26:44 -0300

    Mostrando Tabuleiro

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,13 +9,14 @@ public class Main {
    Jogador2 = USUÁRIO;
    */
 
-  //VARIÁVEIS:
+   //VARIÁVEIS:
    static Scanner input = new Scanner(System.in);
    static String nomeJogador1, nomeJogador2;
    static int tamanhoDoTabuleiroX = 10, tamanhoDoTabuleiroY = 10;
    static int quantidadeDeNavios = 10;
-
-
+   static int tabuleiroJogador1[][], tabuleiroJogador2[][];
+   static int naviosJogador1, naviosJogador2;
+   
    public static void limparTela() throws IOException, InterruptedException {
       new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
    }
@@ -24,7 +26,7 @@ public class Main {
       nomeJogador1 = "COMPUTADOR";
       System.out.println("Digite o seu nome: ");
       nomeJogador2 = input.next();
-
+      
       while (nomeJogador1.equals(nomeJogador2)) {
          System.out.println("");
          System.out.println("O seu nome não pode ser o mesmo da máquina!!!");
@@ -32,13 +34,69 @@ public class Main {
          nomeJogador2 = input.next();
       }
    }
-
+   
    public static int[][] retornarNovoTabuleiroVazio() {
       return new int[tamanhoDoTabuleiroX][tamanhoDoTabuleiroY];
+   }
+   
+   public static void iniciandoTamanhoDosTabuleiros() {
+      tabuleiroJogador1 = retornarNovoTabuleiroVazio();
+      tabuleiroJogador2 = retornarNovoTabuleiroVazio();
+   }
+   
+   public static void instanciarContadoresDeNaviosDosJogadores() {
+      naviosJogador1 = quantidadeDeNavios;
+      naviosJogador2 = quantidadeDeNavios;
+   }
+
+   public static void exibirNumerosDasColunasDosTabuleiros() {
+
+      int numeroDaColuna = 0;
+      String numerosDoTabuleiro = "|   | ";
+
+      for(int i = 0; i < tamanhoDoTabuleiroY; i++) {
+         numerosDoTabuleiro += (numeroDaColuna) + " | ";
+         numeroDaColuna++;
+      }
+      System.out.println(numerosDoTabuleiro);
+   }
+
+   public static void exibirTabuleiro(String nomeDoJogador, int[][] tabuleiro, boolean seuTabuleiro) {
+      System.out.println("---------------------------------------------");
+      System.out.println("              "+ nomeDoJogador +"            ");
+      System.out.println("---------------------------------------------");
+
+      exibirNumerosDasColunasDosTabuleiros();
+
+      System.out.println("---------------------------------------------");
+
+      String linhaDoTabuleiro = "| ";
+      char letraDaLinha = 65;
+
+      for(int[] linha : tabuleiro) {
+         System.out.print("| ");
+         linhaDoTabuleiro = (letraDaLinha++) + " |  ";
+         for (int coluna : linha) {
+            linhaDoTabuleiro += " |  ";
+         }
+         
+         System.out.println(linhaDoTabuleiro);
+         System.out.println("---------------------------------------------");
+      }
+   }
+
+   public static void exibirTabuleiroDosJogadores() {
+      exibirTabuleiro(nomeJogador1, tabuleiroJogador1, true);
+      System.out.println("");
+      exibirTabuleiro(nomeJogador2, tabuleiroJogador2, false);
    }
 
    public static void main(String[] args) throws Exception {
       limparTela();
       obterNomeDoJogador();
+      iniciandoTamanhoDosTabuleiros();
+      instanciarContadoresDeNaviosDosJogadores();
+      exibirTabuleiroDosJogadores();
+
    }
 }
