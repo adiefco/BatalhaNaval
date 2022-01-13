051a0b6854acc5e7d77ebeacd42c1b91c3bd3668
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2022-01-13 18:32:26 -0300

    variavel quantidade de navios

--- VariaveisGlobais.java
+++ VariaveisGlobais.java
@@ -13,7 +13,7 @@ public class VariaveisGlobais {
 
    public static int tamanhoDoTabuleiroX = 10;
    public static int tamanhoDoTabuleiroY = 10;
-   public static int quantidadeDeNavios = 1;
+   public static int quantidadeDeNavios = 10;
    public static int[][] tabuleiroJogador1 = new int[tamanhoDoTabuleiroX][tamanhoDoTabuleiroY];
    public static int[][] tabuleiroJogador2 = new int[tamanhoDoTabuleiroX][tamanhoDoTabuleiroY];
    public static int naviosJogador1 = quantidadeDeNavios;
